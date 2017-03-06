790b53ef5308c26fef1c0ccad3c92f5df61a6396
wangyue <wangyue08@meituan.com>
2017-03-06 19:57:34 +0800

    add dollar and underscore

--- src/bnf/IdentifierStart.js
+++ src/bnf/IdentifierStart.js
@@ -4,10 +4,14 @@ module.exports = {
   rules: [
     `UnicodeIDStart`,
     `UnicodeEscapeSequence`,
+    `$`,
+    `_`,
   ],
   handlers: [
     `$$ = $1;`,
     `$$ = $1;`, // TODO: unicode with id start
+    `$$ = $1;`,
+    `$$ = $1;`,
   ],
   subRules: [
     require('./UnicodeEscapeSequence'),
