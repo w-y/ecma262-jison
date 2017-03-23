52b1d1aea05d3235466a68703d227eae60537825
wangyue <wangyue08@meituan.com>
2017-03-23 21:47:13 +0800

    try to add multiline comment

--- src/lex/comment.js
+++ src/lex/comment.js
@@ -1,5 +1,67 @@
+const MultiLineCommentCharsStart = {
+  conditions: ['INITIAL'],
+  rule: '/\\*',
+  handler: `
+    this.begin('multi_line_comment_start');
+    this.comment = {
+      range: [
+        [yylloc.first_line, yylloc.first_column],
+      ],
+      type: 'MuliLine',
+      value: [],
+    };
+    return '';
+  `,
+};
+
+const MultiLineCommentCharsEnd = {
+  conditions: ['multi_line_comment_start'],
+  rule: '\\*/',
+  handler: `
+    this.popState();
+    return '';
+  `,
+};
+
+const MultiLineNotAsteriskChar = {
+  conditions: ['multi_line_comment_start'],
+  rule: '[^*]',
+  handler: `
+    console.log('===============');
+    console.log(this.match);
+    console.log('===============');
+    return '';
+  `,
+};
+
+const PostAsteriskCommentCharsStart = {
+  conditions: ['multi_line_comment_start'],
+  rule: '[*]',
+  handler: `
+    this.begin('multi_line_comment_post_asterisk_start');
+    return '';
+  `,
+};
+
+const PostAsteriskCommentChars = {
+  conditions: ['multi_line_comment_post_asterisk_start'],
+  rule: '.',
+  handler: `
+    if (this.match === '*') {
+      this.begin('multi_line_comment_post_asterisk_start');
+      return '';
+    } else if (this.match === '/') {
+      ;  
+    } else {
+      // MultiLineNotForwardSlashOrAsteriskChar
+      this.popState();
+    }
+    return '';
+  `,
+};
+
 const SingleLineCommentCharsStart = {
-  conditions: ['*'],
+  conditions: ['INITIAL'],
   rule: '//',
   handler: `
     this.begin('single_line_comment_start');
@@ -29,4 +91,9 @@ const SingleLineCommentChar = {
 exports.singleLineComment = [
   SingleLineCommentCharsStart,
   SingleLineCommentChar,
+];
+
+exports.multiLineComment = [
+  MultiLineCommentCharsStart,
+  MultiLineNotAsteriskChar,
 ];
