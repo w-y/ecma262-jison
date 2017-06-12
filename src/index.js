16d617800da930ce7c35ef085d2d766672835255
w-y <wang.wang.yue@qq.com>
2017-06-12 12:01:11 +0800

    fix eslint

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 const autoinsertion = require('./autoinsertion');
 
-module.exports.parse = function(src, opts) {
-
+module.exports.parse = function (src, opts) {
   // NOTICE:
   // add a Line Terminator manually
   // change statement like a to a\n which can apply the automatcic insertion rule
 
-  return autoinsertion(src + '\n', opts);
-}
+  return autoinsertion(`${src}\n`, opts);
+};
